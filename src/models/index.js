a17aa20ec3f1412852f7a258930359ee7a95defd
PedroTrudes <pedro.jtrudes@gmail.com>
2024-09-13 20:10:29 -0300

    :sparkles: feat: validacao globais no mongoose

---
+++ src/models/index.js
@@ -0,0 +1,7 @@
+import "./validadorGlobal.js";
+import autores from "./Autor.js";
+import livros from "./Livro.js";
+
+export { autores, livros }
+
+//Agora os arquivos de models são importados por aqui, pois vamos fazer validacoes globais na nossa api
